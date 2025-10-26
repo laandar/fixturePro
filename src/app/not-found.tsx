dae263db1ab475a6180356314faf8bdde09e1de1
Darwin Sinche <darwin.sinche@gmail.com>
2025-10-26 07:39:17 -0500

    Fix conditional logic for player photos and initials

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -30,7 +30,7 @@ const Page = () => {
             </div>
 
             <p className="text-center text-muted mt-5 mb-0">
-              © 2014 - {currentYear} FIXTUREPRO — by <span className="fw-bold">{author}</span>
+              © {currentYear} FIXTUREPRO — by <span className="fw-bold">{author}</span>
             </p>
           </Col>
         </Row>
